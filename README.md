cf95b7a77382913b6b5b6ba50fbd9607b8651bb2
Vinicius-B-Leite <viniciusbleite21@gmail.com>
2022-08-26 09:53:12 -0300

    readme adicionado

---
+++ README.md
@@ -0,0 +1,39 @@
+# Clima App
+
+Aplicativo que exibe os dados atuais do clima em uma determinada região informada pelo usuário.
+
+## Tecnologias
+>
+Aqui está as tecnologias usadas no projeto
+
+ - React Native 0.69.4
+ - Expo 46.0.8
+ - Axios 0.27.2
+ - Styled Components 5.3.5
+ - TypeScript 4.3.5
+ 
+ ## Serviços usados
+  - Github
+
+## Como usar
+
+### Apresentação
+  ![início](github/img1.jpeg)
+  ![cidade2](github/img2.jpeg)
+  ![cidade3](github/img3.jpeg)
+
+## Funcionalidades
+
+A principal funcionalidade é:
+ - Exibir os dados do clima atual a partir da requisoção à API OpenWeatherApp
+
+## Links
+ - Repositório: https://github.com/Vinicius-B-Leite/Clima-app
+ - Em casos de bugs, por favor me contate
+  viniciusbleite21@gmail.com
+
+## Versão
+ 1.0.0
+
+## Autor
+Feito com <3 por Vinicius B. Leite
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {Contaner, Imagem, Local, ConteinerInfo, Temperatura, Info, InfoTexto, InforSubTexto, ContanerPrincipal, Descricao} from './styles.js'
+import { BlurView } from 'expo-blur';
+import blurViewStyle from "./blurViewStyle.js";
+
+
+function Home() {
+    return (  
+        <Contaner>
+            <Imagem source={require("../../images/chuva.jpg")}/>
+            <ConteinerInfo>
+                <ContanerPrincipal>
+                    <Local>Atibaia, SP, Brasil</Local>
+                    <Temperatura cor="#f2f2">22°</Temperatura>
+                    <Descricao>Nublado</Descricao>
+                </ContanerPrincipal>
+
+                    <BlurView intensity={100} tint='default' style={blurViewStyle.blurConteiner}> 
+            
+                        <Info>
+                            <InfoTexto>64%</InfoTexto>
+                            <InforSubTexto>Umidade</InforSubTexto>
+                        </Info>
+                        <Info>
+                            <InfoTexto>4km</InfoTexto>
+                            <InforSubTexto>Vento</InforSubTexto>
+                        </Info>
+                        <Info>
+                            <InfoTexto>25°</InfoTexto>
+                            <InforSubTexto>Máxima</InforSubTexto>
+                        </Info>
+                    </BlurView>
+            </ConteinerInfo>
+        </Contaner>
+    );
+}
+
+export default Home;
